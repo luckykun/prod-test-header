3c731f604d95d24763d88037f9a174bb3f7f0eb1
zhikun.jzk <zhikun.jzk@alibaba-inc.com>
2021-07-08 14:54:56 +0800

    module federation

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div
+    style={{
+      padding: 20,
+      background: 'lightblue'
+    }}
+    >
+      Header 1.0
+    </div>
+  )
+}
